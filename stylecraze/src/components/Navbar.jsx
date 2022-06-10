b2fc1830d068f6b40c4c4bf3fefaadbcaeb7bb88
Santanu55555 <99542723+Santanu55555@users.noreply.github.com>
2022-06-10 10:08:42 +0530

    k

---
+++ stylecraze/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <div><img src='https://cdn2.stylecraze.com/wp-content/themes/buddyboss-child/images/sc-logo.png'/></div>
+        <div></div>
+    </div>
+  )
+}
